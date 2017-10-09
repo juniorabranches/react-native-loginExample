8a21da231a22872a29db85aa911a644104998712
Jill Cates <cates.jill@gmail.com>
2017-10-09 08:29:33 -0700

    Add validation to registration/cadastro form

--- src/components/cadastro/cadastroForm.js
+++ src/components/cadastro/cadastroForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {  View, TextInput, StyleSheet } from 'react-native';
 import { Dropdown } from 'react-native-material-dropdown';
+import { Form, FormItem } from 'react-native-form-validation';
 
 export default class CadastroForm extends Component {
     render() {
@@ -11,48 +12,62 @@ export default class CadastroForm extends Component {
       }];
       return  (
         <View style={styles.container}>
-          <TextInput style = {styles.input}
-                         autoCapitalize="none"
-                         //onSubmitEditing={() => this.dropDown.focus()}
-                         ref={(input)=> this.nameInput = input}
-                         autoCorrect={false}
-                         keyboardType='default'
-                         returnKeyType="next"
-                         placeholder='Nome do usuário'
-                         placeholderTextColor="#000"/>
-
-         <Dropdown style = {styles.dropDown}
-           ref={(input)=> this.dropDown = input}
-           label='Selecione sua profissão'
-           textColor="#000"
-           baseColor="#000"
-           data={data}
-         />
-
-         <TextInput style = {styles.input}
-                        autoCapitalize="none"
-                        onSubmitEditing={() => this.passwordInput.focus()}
-                        ref={(input)=> this.emailInput = input}
-                        autoCorrect={false}
-                        keyboardType='email-address'
-                        returnKeyType="next"
-                        placeholder='E-mail'
-                        placeholderTextColor="#000"/>
-
-           <TextInput style = {styles.input}
-                         returnKeyType="next"
-                         onSubmitEditing={() => this.newPasswordInput.focus()}
-                         ref={(input)=> this.passwordInput = input}
-                         placeholder='Digite sua senha'
-                         placeholderTextColor="#000"
-                         secureTextEntry/>
-
-           <TextInput style = {styles.input}
-                         returnKeyType="go"
-                         ref={(input)=> this.newPasswordInput = input}
-                         placeholder='Confirme sua senha'
-                         placeholderTextColor="#000"
-                         secureTextEntry/>
+          <Form
+            ref='form'
+            shouldValidate={true}>
+            <FormItem
+              isRequired={true}>
+                <TextInput style = {styles.input}
+                              autoCapitalize="none"
+                              //onSubmitEditing={() => this.dropDown.focus()}
+                              ref={(input)=> this.nameInput = input}
+                              autoCorrect={false}
+                              keyboardType='default'
+                              returnKeyType="next"
+                              placeholder='Nome do usuário'
+                              placeholderTextColor="#000"/>
+            </FormItem>
+            <FormItem
+              isRequired={true}>
+              <Dropdown style = {styles.dropDown}
+                ref={(input)=> this.dropDown = input}
+                label='Selecione sua profissão'
+                textColor="#000"
+                baseColor="#000"
+                data={data} />
+            </FormItem>
+            <FormItem
+              isRequired={true}>
+            <TextInput style = {styles.input}
+                            autoCapitalize="none"
+                            onSubmitEditing={() => this.passwordInput.focus()}
+                            ref={(input)=> this.emailInput = input}
+                            autoCorrect={false}
+                            keyboardType='email-address'
+                            returnKeyType="next"
+                            placeholder='E-mail'
+                            placeholderTextColor="#000"/>
+            </FormItem>
+            <FormItem
+              isRequired={true}>
+              <TextInput style = {styles.input}
+                            returnKeyType="next"
+                            onSubmitEditing={() => this.newPasswordInput.focus()}
+                            ref={(input)=> this.passwordInput = input}
+                            placeholder='Digite sua senha'
+                            placeholderTextColor="#000"
+                            secureTextEntry/>
+            </FormItem>
+            <FormItem
+              isRequired={true}>
+              <TextInput style = {styles.input}
+                            returnKeyType="go"
+                            ref={(input)=> this.newPasswordInput = input}
+                            placeholder='Confirme sua senha'
+                            placeholderTextColor="#000"
+                            secureTextEntry/>
+            </FormItem>
+          </Form>
         </View>
       );
     }
